9bc99c78c29e30a01c82ff179d6dc11f7d10adf5
Breno Lipi <brenorlipi@hotmail.com>
2023-09-27 19:45:39 -0300

    fix(types): remove unnecessary types

--- src/types.d.ts
+++ src/types.d.ts
@@ -7,17 +7,8 @@ interface StarshipsData {
 
 interface Starship {
     name: string;
-    model: string;
-    manufacturer: string;
-    cost_in_credits: string;
-    length: string;
-    max_atmosphering_speed: string;
-    crew: string;
-    passengers: string;
-    cargo_capacity: string;
     consumables: string;
-    hyperdrive_rating: string;
     MGLT: string;
-    starship_class: string;
+    number_of_stops: string;
     url: string;
 }

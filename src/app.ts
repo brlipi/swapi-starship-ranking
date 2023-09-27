9dbf02ff8cef5e5533966f35e65b72834390d70d
Breno Lipi <brenorlipi@hotmail.com>
2023-09-27 18:54:22 -0300

    feat: Add swagger to project

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express"
 import router from "./Routes";
 
 const port = process.env.PORT || 8000;
@@ -8,6 +9,12 @@ const app: Application = express();
 
 app.use(morgan('common'));
 app.use(express.static('public'));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(undefined, {
+    swaggerOptions: {
+        url: '/swagger.json',
+    },
+}));
+
 
 app.use(router);
 

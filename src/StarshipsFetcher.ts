154e0e49699b098051d845097a64dd269ef11882
Breno Lipi <brenorlipi@hotmail.com>
2023-09-27 19:45:04 -0300

    fix(fetcher): simplify logic for faster fetching

--- src/StarshipsFetcher.ts
+++ src/StarshipsFetcher.ts
@@ -3,15 +3,25 @@ import axios from "axios";
 export class StarshipsFetcher {
     public async getAllStarships(): Promise<Starship[]> {
         try {
-            let count: number = await this.getStarshipsCount();
-            let next: string | null = 'https://swapi.dev/api/starships';
+            let count: number;
+            let first: string = 'https://swapi.dev/api/starships';
+            let next: string | null = '';
+            let data: StarshipsData;
             let current: Starship[], starships: Starship[] = [];
-            do {
-                current = await this.getStarships(next);
+
+            data = await this.fetchData(first);
+            next = data.next;
+            current = data.results;
+            count = data.count - current.length;
+            starships = starships.concat(current);
+
+            while (count > 0 && next !== null) {
+                data = await this.fetchData(next);
+                next = data.next;
+                current = data.results;
                 count -= current.length;
                 starships = starships.concat(current);
-                next = await this.getStarshipsNext(next);
-            } while (count > 0 && next !== null);
+            }
             return starships;
         } catch (error) {
             console.log(error);
@@ -19,39 +29,14 @@ export class StarshipsFetcher {
         }
     }
 
-    protected async getStarshipsCount(): Promise<number> {
-        try {
-            const response = await axios.get('https://swapi.dev/api/starships');
-            const data: StarshipsData = response.data;
-            const count: number = data.count;
-            return count;
-        } catch (error) {
-            console.log(error);
-            throw (error);
-        }
-    }
-
-    protected async getStarshipsNext(current: string): Promise<string | null> {
-        try {
-            const response = await axios.get(current);
-            const data: StarshipsData = response.data;
-            const next = data.next;
-            return next;
-        } catch (error) {
-            console.log(error);
-            throw (error);
-        }
-    }
-
-    protected async getStarships(url: string): Promise<Starship[]> {
+    protected async fetchData(url: string): Promise<StarshipsData> {
         try {
             const response = await axios.get(url);
             const data: StarshipsData = response.data;
-            const starships: Starship[] = data.results;
-            return starships;
+            return data;
         } catch (error) {
             console.log(error);
-            throw (error);
+            throw(error);
         }
     }
 }
